03a95916f9c059565f0e781f76a90ffe41b9ae43
Daniel Bast <2790401+dbast@users.noreply.github.com>
2017-12-10 11:51:02 +0100

    Move ci.sh steps into functions

--- ci.sh
+++ ci.sh
@@ -5,21 +5,40 @@ set -o errtrace -o nounset -o pipefail -o errexit
 # Goto directory of this script
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-echo "Executing self-check"
-# Don't fail here, failing later at the end when all shell scripts are checked anyway.
-shellcheck ./ci.sh && echo "Self-check succeeded!" || echo "Self-check failed!"
+self_check () {
+  echo "Executing self-check"
+  # Don't fail here, failing later at the end when all shell scripts are checked anyway.
+  shellcheck ./ci.sh && echo "Self-check succeeded!" || echo "Self-check failed!"
+}
 
-echo "Cleaning"
-./sbt clean
+cleaning () {
+  echo "Cleaning"
+  ./sbt clean
+}
 
-echo "Executing unit tests"
-./sbt coverage test
+unit_tests () {
+  echo "Executing unit tests"
+  ./sbt coverage test
+}
 
-echo "Executing integration tests"
-./sbt coverage it:test
+integration_tests () {
+  echo "Executing integration tests"
+  ./sbt coverage it:test
+}
 
-echo "Generating combined coverage report"
-./sbt coverageReport
+coverage_report () {
+  echo "Generating combined coverage report"
+  ./sbt coverageReport
+}
 
-echo "Executing shellcheck against all included shell scripts"
-find . -name "*.sh" -print0 | xargs -n 1 -0 shellcheck
+shell_check () {
+  echo "Executing shellcheck against all included shell scripts"
+  find . -name "*.sh" -print0 | xargs -n 1 -0 shellcheck
+}
+
+self_check
+cleaning
+unit_tests
+integration_tests
+coverage_report
+shell_check
